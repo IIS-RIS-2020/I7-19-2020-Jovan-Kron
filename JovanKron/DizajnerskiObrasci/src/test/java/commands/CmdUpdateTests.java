9cdc82ea9ae58385b86bb0c0910e2835ff5af825
krjovan <jovankron@gmail.com>
2021-05-12 12:37:32 +0200

    Tests for update, set edge and fill color commands

---
+++ JovanKron/DizajnerskiObrasci/src/test/java/commands/CmdUpdateTests.java
@@ -0,0 +1,50 @@
+package commands;
+
+import static org.junit.Assert.assertEquals;
+
+import java.awt.Color;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import geometry.Circle;
+import geometry.Point;
+import mvc.DrawingModel;
+
+public class CmdUpdateTests {
+	
+	private CmdUpdate cmdUpdate;
+	private DrawingModel model;
+	private Circle originalCircle;
+
+	@Before
+	public void setUp() {
+		model = new DrawingModel();
+		originalCircle = new Circle(new Point(1, 1), 10, Color.RED, Color.YELLOW);
+		model.add(originalCircle);
+		Circle updatedCircle = new Circle(new Point(5, 5), 50, Color.BLACK, Color.GREEN);
+		cmdUpdate = new CmdUpdate(originalCircle, updatedCircle);
+		cmdUpdate.setModel(model);
+	}
+	
+	@Test
+	public void testExecuteExpectedTrue() {
+		cmdUpdate.execute();
+		assertEquals(5, originalCircle.getCenter().getX());
+		assertEquals(5, originalCircle.getCenter().getY());
+		assertEquals(50, originalCircle.getRadius());
+		assertEquals(Color.BLACK, originalCircle.getEdgeColor());
+		assertEquals(Color.GREEN, originalCircle.getFillColor());
+	}
+
+	@Test
+	public void testUnexecuteExpectedTrue() {
+		cmdUpdate.execute();
+		cmdUpdate.unexecute();
+		assertEquals(1, originalCircle.getCenter().getX());
+		assertEquals(1, originalCircle.getCenter().getY());
+		assertEquals(10, originalCircle.getRadius());
+		assertEquals(Color.RED, originalCircle.getEdgeColor());
+		assertEquals(Color.YELLOW, originalCircle.getFillColor());
+	}
+}
